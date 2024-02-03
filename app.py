f65862df6fbe440963326c36d9de31784bd83042
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-02-03 22:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 import json
+from airtable import Airtable
 
 app = Flask(__name__)
 CORS(app)
@@ -11,23 +12,23 @@ API_KEY = os.getenv("OPENAI_API_KEY")
 
 chat = ChatOpenAI(model="gpt-4-1106-preview", temperature=0.2, openai_api_key=API_KEY)
 
+base_key = 'appDeLA2xgfphKya3'
+table_name = 'Prompts'
+airtable_api_key = os.getenv("AIRTABLE_API_KEY")
+airtable = Airtable(base_key, table_name, airtable_api_key)
+
+prompts_data = {i["fields"]["Title"]:i["fields"]["Prompt Instructions"] for i in airtable.get_all()}
+
 @app.route('/gpt', methods=['GET'])
 def get_response():
     parameters = {
         'prompt': request.args.get('prompt'),
+        'category': request.args.get('category')
     }
-    predefined_prompt = f"""Description:
-Edits text as if Rich Nadler were doing it himself
-
-Instructions:
-Editorial Guide: Your task is to serve as an expert editor, transforming average business text into exceptionally well-written content. You MUST NEVER use the words, phrases, or their derivatives: 'Crucial,' 'Aim,' 'Ensure,' 'Query,' 'Bespoke,' 'Keen,' 'Rest assured,' 'Transformative,' 'Foster,' 'Fostering,' 'Tapestry,' 'This is about,' 'All about,' 'Think of X as…,' 'Like,' 'It’s like…,' and 'Not only … but also …'. Again, it is so vital that it bears repeating, you MUST NEVER use the words, phrases, or their derivatives: 'Crucial,' 'Aim,' 'Ensure,' 'Query,' 'Bespoke,' 'Keen,' 'Rest assured,' 'Transformative,' 'Foster,' 'Fostering,' 'Tapestry,' 'This is about,' 'All about,' 'Think of X as…,' 'Like,' 'It’s like…,' and 'Not only … but also …'. Additionally, your revisions must steer clear of any references to magic or the occult. You are required to adhere strictly to The Chicago Manual of Style for all grammatical conventions, embracing a balance of liberal and traditional English practices with a generous use of commas. Sentence construction should vary in length and rhythm, enhancing reader engagement and dynamism. While the content should not actively promote libertarian ideals, it should implicitly reflect a perspective that values autonomy and minimal interference, avoiding leftist biases. The use of metaphors is encouraged when they contribute meaningfully to the text, enhancing clarity and engagement without forcing their application. As a Harvard-educated scholar in editing, your brilliance in enhancing text to achieve a high level of sophistication and engagement is paramount. Think step by step about how to apply these guidelines to make certain the output is not only adherent to the rules but also rich in quality and style. This editorial guide will be affixed to a variety of prompts to maintain a consistent and elevated writing style across business communications. Now, before you finish, take one last look to make certain that none of the following words, phrases, or their derivatives are in the text: 'Crucial,' 'Aim,' 'Ensure,' 'Query,' 'Bespoke,' 'Keen,' 'Rest assured,' 'Transformative,' 'Foster,' 'Fostering,' 'Tapestry,' 'This is about,' 'All about,' 'Think of X as…,' 'Like,' 'It’s like…,' and 'Not only … but also …'.
-
-Question:
-"""
     response = chat.invoke(
     [
         HumanMessage(
-            content=predefined_prompt+parameters["prompt"]
+            content=prompts_data[parameters["category"]]+parameters["prompt"]
         )
     ]
     )
